e4daf0cf7cb1f1a27c17f16a8d8234f0cc18f536
Jonathan Morales <jonathansme01@gmail.com>
2026-06-15 14:27:06 -0600

    The structure and aesthetics of the form are defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Form from "./components/Form";
+
 function App() {
   return (
     <>
@@ -6,8 +8,15 @@ function App() {
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorias
           </h1>
+          <button></button>
         </div>
       </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form />
+        </div>
+      </section>
     </>
   );
 }
